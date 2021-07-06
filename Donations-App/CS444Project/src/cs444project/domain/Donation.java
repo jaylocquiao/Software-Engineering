a8594495e8050e2acd45209109d845c002852ef2
jaylocquiao <60939866+jaylocquiao@users.noreply.github.com>
2021-07-06 07:46:18 -1000

    Update Donations.java
    Removed unnecessary comments.

--- Donations-App/CS444Project/src/cs444project/domain/Donation.java
+++ Donations-App/CS444Project/src/cs444project/domain/Donation.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs444project.domain;
 
 import java.time.LocalDate;
@@ -65,15 +60,12 @@ public class Donation {
     }
     
     public boolean validateDonation() {
-        //DONATIONTITLEFLD
-        if (donationTitle.equals("")) { //up to user what they want to name their donation
+        if (donationTitle.equals("")) {
             return false;
         }
-        //DONATIONTYPEFLD
         if (donationType.equals("")) {
             return false;
         }
-        //DONATIONVALUEFLD
         if (donationValue.equals("")) {
             return false;
         }
